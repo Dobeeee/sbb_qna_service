4cd336c658f242cd47f1782a028b49a4993225c8
Dobeeee <wsh0684@nate.com>
2024-01-18 15:28:40 +0900

    GetMapping, PostMapping, RequestMapping의 차이

--- src/main/java/com/example/sbb/controller/MainController.java
+++ src/main/java/com/example/sbb/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.example.sbb.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -11,6 +10,26 @@ public class MainController {
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
     public String index() {
-        return "안녕하세요 테스트";
+        return "안녕하세요 되냥";
+    }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showGet() {
+        return """
+                <form action="/page2" method="POST">
+                    <input type="text" name="age" placeholder="숫자 입력"/>
+                    <input type="submit" value="page2" />
+                </form>    
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPost(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 숫자 : %d</h1>
+                <h1>page2 POST 방식입니다.</h1>
+                """.formatted(age);
     }
 }
